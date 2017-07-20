0a31dc54b61bc87c9b3bb2b8a35d248884af41ab
LexaTavares <alexis.tavares.chavez@gmail.com>
2017-07-20 15:56:10 -0500

    parte 2

--- assets/js/yape.js
+++ assets/js/yape.js
@@ -1,25 +1,45 @@
-$(document).ready(carrgarPagina);
-
-function carrgarPagina() {
-  $('.slider').slider();
+function cargarPagina() {
   $('.carousel.carousel-slider').carousel();
-  $('.numeroTel').keyup(validarContenido);
-  $('.box').click(checkboxTrue);
+  $('.box').change(validarContenido);
+  $('#agregar').click(moverIngresaCod);
+  datosRecibidos();
 };
 
-var botonAdd = $('#agregar');
 function validarContenido() {
-
-
-  if($('.numeroTel').val().length == 9)
-  {
-    console.log("numero de telefono correcto");
-  };
-};
-function checkboxTrue(){
-  if($('.box').is(':checked')){
+  var botonAdd = $("#agregar");
+  if ($(".numeroTel").val().length == 10 && $(".box").prop("checked")) {
     botonAdd.removeAttr("disabled");
+    window.location
   } else {
     botonAdd.attr("disabled", true);
   }
 };
+
+function moverIngresaCod() {
+  $(location).attr('href', 'http://localhost:3000/views/ingresaCod.html')
+
+}
+
+
+var api = {
+  url: "http://localhost:3000/api/registerNumber"
+}
+
+function datosRecibidos() {
+  $.post(api.url, {
+      "phone": 7412589630,
+      "terms": true,
+      "code": "code"
+    },
+    function(datoRecibido) {
+      console.log(datoRecibido);
+      alert(datoRecibido.data.code);
+      // if ($('#code').val().length == 6) {
+      //   $(location).attr('href', 'http://localhost:3000/views/usuarioYape.html')
+      // };
+    });
+
+}
+
+
+$(document).ready(cargarPagina);
